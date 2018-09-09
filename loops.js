7908bd00274735249a01f50f8ebca0e44634c5f7
Kara Isreal <isreal.kara92@gmail.com>
2018-09-09 13:18:27 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,18 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+  if (i === 1) {
+    array.push("I am 1 strange loop.")
+  } else {
+array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
+  }
+}
+return array
+}
+
+let countdown = 100;
+function whileLoop(n) {
+  while ((countdown) => 0) {
+    console.log(--countdown);
+  }
+  return ('done')
+}
